621e948311a06e045c547bc5f1efd8cad6cd0b7a
Nana Sakisaka <1901813+saki7@users.noreply.github.com>
2026-02-04 19:11:39 +0900

    Rename `Ts` to `Cs` for CTP

--- include/iris/type_traits.hpp
+++ include/iris/type_traits.hpp
@@ -24,8 +24,8 @@ inline constexpr bool is_ttp_specialization_of_v = is_ttp_specialization_of<T, T
 template<class T, template<auto...> class TT>
 struct is_ctp_specialization_of : std::false_type {};
 
-template<template<auto...> class TT, auto... Ts>
-struct is_ctp_specialization_of<TT<Ts...>, TT> : std::true_type {};
+template<template<auto...> class TT, auto... Cs>
+struct is_ctp_specialization_of<TT<Cs...>, TT> : std::true_type {};
 
 template<class T, template<auto...> class TT>
 inline constexpr bool is_ctp_specialization_of_v = is_ctp_specialization_of<T, TT>::value;
