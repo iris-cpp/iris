e1da465435aa6c61daa34121d88df287a0a3229f
Nana Sakisaka <1901813+saki7@users.noreply.github.com>
2026-02-04 17:21:36 +0900

    Add minimal implementation of config and type_traits

---
+++ include/iris/type_traits.hpp
@@ -0,0 +1,31 @@
+#ifndef IRIS_TYPE_TRAITS_HPP
+#define IRIS_TYPE_TRAITS_HPP
+
+#include <iris/config.hpp>
+
+#include <type_traits>
+
+namespace iris {
+
+template<class T, template<class...> class TT>
+struct is_ttp_specialization_of : std::false_type {};
+
+template<template<class...> class TT, class... Ts>
+struct is_ttp_specialization_of<TT<Ts...>, TT> : std::true_type {};
+
+template<class T, template<class...> class TT>
+inline constexpr bool is_ttp_specialization_of_v = is_ttp_specialization_of<T, TT>::value;
+
+
+template<class T, template<auto...> class TT>
+struct is_nttp_specialization_of : std::false_type {};
+
+template<template<auto...> class TT, auto... Ts>
+struct is_nttp_specialization_of<TT<Ts...>, TT> : std::true_type {};
+
+template<class T, template<auto...> class TT>
+inline constexpr bool is_nttp_specialization_of_v = is_nttp_specialization_of<T, TT>::value;
+
+} // iris
+
+#endif
---
+++ test/test.hpp
@@ -0,0 +1,8 @@
+#ifndef IRIS_TEST_TEST_HPP
+#define IRIS_TEST_TEST_HPP
+
+#include <iris/config.hpp>
+
+#include <catch2/catch_test_macros.hpp>
+
+#endif
