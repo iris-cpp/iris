ed72668a08faf40c197c64fd13a8ad237c90f199
Nana Sakisaka <1901813+saki7@users.noreply.github.com>
2026-03-05 17:38:52 +0900

    Fix `StringLike` for non-string-like range

--- include/iris/string.hpp
+++ include/iris/string.hpp
@@ -3,15 +3,27 @@
 
 // SPDX-License-Identifier: MIT
 
+#include <concepts>
 #include <string>
 #include <string_view>
 
 namespace iris {
 
 template<class T>
-concept StringLike = requires(T t) {
-    std::basic_string_view{t};
-};
+concept CharLike =
+    std::same_as<T, char> ||
+    std::same_as<T, char32_t> ||
+    std::same_as<T, wchar_t> ||
+    std::same_as<T, char8_t> ||
+    std::same_as<T, char16_t>;
+
+template<class T>
+concept StringLike =
+    std::convertible_to<T, std::basic_string_view<char>> ||
+    std::convertible_to<T, std::basic_string_view<char32_t>> ||
+    std::convertible_to<T, std::basic_string_view<wchar_t>> ||
+    std::convertible_to<T, std::basic_string_view<char8_t>> ||
+    std::convertible_to<T, std::basic_string_view<char16_t>>;
 
 template<class T>
 concept NotStringLike = !StringLike<T>;
