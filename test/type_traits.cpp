37bf9534bfb22625885170abc5c7b75393471376
yaito3014 <ykakeyama3014@gmail.com>
2026-03-13 00:41:21 +0900

    Add test

--- test/type_traits.cpp
+++ test/type_traits.cpp
@@ -19,7 +19,73 @@ struct n_tuple;
 template<int... Ns>
 struct n_list;
 
+struct convertible_from_int {
+    convertible_from_int(int);
+};
+
+struct not_convertible_from_int {};
+
+enum class scoped_enum {};
+enum unscoped_enum {};
+
 } // anonymous
+
+
+TEST_CASE("is_convertible_without_narrowing")
+{
+    // Same type: always OK
+    STATIC_CHECK(iris::is_convertible_without_narrowing_v<int, int>);
+    STATIC_CHECK(iris::is_convertible_without_narrowing_v<double, double>);
+
+    // Widening integer conversions: OK
+    STATIC_CHECK(iris::is_convertible_without_narrowing_v<int, long long>);
+    STATIC_CHECK(iris::is_convertible_without_narrowing_v<short, int>);
+    STATIC_CHECK(iris::is_convertible_without_narrowing_v<char, int>);
+    STATIC_CHECK(iris::is_convertible_without_narrowing_v<unsigned, unsigned long>);
+
+    // Narrowing integer conversions: rejected
+    STATIC_CHECK(!iris::is_convertible_without_narrowing_v<long long, int>);
+    STATIC_CHECK(!iris::is_convertible_without_narrowing_v<int, short>);
+    STATIC_CHECK(!iris::is_convertible_without_narrowing_v<int, char>);
+    STATIC_CHECK(!iris::is_convertible_without_narrowing_v<unsigned long, unsigned>);
+
+    // Floating-point widening: OK
+    STATIC_CHECK(iris::is_convertible_without_narrowing_v<float, double>);
+    STATIC_CHECK(iris::is_convertible_without_narrowing_v<float, long double>);
+    STATIC_CHECK(iris::is_convertible_without_narrowing_v<double, long double>);
+
+    // Floating-point narrowing: rejected
+    STATIC_CHECK(!iris::is_convertible_without_narrowing_v<double, float>);
+    STATIC_CHECK(!iris::is_convertible_without_narrowing_v<long double, double>);
+
+    // Integer to floating-point: narrowing
+    STATIC_CHECK(!iris::is_convertible_without_narrowing_v<int, float>);
+    STATIC_CHECK(!iris::is_convertible_without_narrowing_v<long long, double>);
+
+    // Floating-point to integer: narrowing
+    STATIC_CHECK(!iris::is_convertible_without_narrowing_v<float, int>);
+    STATIC_CHECK(!iris::is_convertible_without_narrowing_v<double, int>);
+
+    // Signed/unsigned mismatch
+    STATIC_CHECK(!iris::is_convertible_without_narrowing_v<int, unsigned>);
+    STATIC_CHECK(!iris::is_convertible_without_narrowing_v<unsigned, int>);
+
+    // Bool conversions
+    STATIC_CHECK(iris::is_convertible_without_narrowing_v<bool, int>);
+    STATIC_CHECK(!iris::is_convertible_without_narrowing_v<int, bool>);
+
+    // User-defined implicit conversion: OK (no narrowing concern)
+    STATIC_CHECK(iris::is_convertible_without_narrowing_v<int, convertible_from_int>);
+
+    // Not convertible at all
+    STATIC_CHECK(!iris::is_convertible_without_narrowing_v<int, not_convertible_from_int>);
+    STATIC_CHECK(!iris::is_convertible_without_narrowing_v<int, std::string>);
+
+    // Enum conversions
+    STATIC_CHECK(!iris::is_convertible_without_narrowing_v<int, scoped_enum>);
+    STATIC_CHECK(!iris::is_convertible_without_narrowing_v<scoped_enum, int>);
+    STATIC_CHECK(iris::is_convertible_without_narrowing_v<unscoped_enum, int>);
+}
 
 
 TEST_CASE("specialization_of")
