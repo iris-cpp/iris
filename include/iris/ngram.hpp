72ca69163f2fb21d40f7bc681876134650a4aa67
Nana Sakisaka <1901813+saki7@users.noreply.github.com>
2026-08-11 19:44:27 +0900

    Implement N-gram indexer

---
+++ include/iris/ngram.hpp
@@ -0,0 +1,163 @@
+#ifndef IRIS_NGRAM_HPP
+#define IRIS_NGRAM_HPP
+
+#include <format>
+#include <span>
+#include <flat_map>
+#include <string>
+#include <string_view>
+#include <vector>
+#include <array>
+#include <ranges>
+#include <algorithm>
+#include <type_traits>
+#include <concepts>
+#include <utility>
+#include <compare>
+
+#include <cassert>
+
+namespace iris {
+
+enum struct ngram_document_id : unsigned {};
+
+} // iris
+
+template<class CharT>
+struct std::formatter<iris::ngram_document_id, CharT>
+    : std::formatter<std::underlying_type_t<iris::ngram_document_id>, CharT>
+{
+    using base_type = std::formatter<std::underlying_type_t<iris::ngram_document_id>, CharT>;
+
+    template <class Ctx>
+    Ctx::iterator format(iris::ngram_document_id doc_id, Ctx& ctx) const
+    {
+        return base_type::format(std::to_underlying(doc_id), ctx);
+    }
+};
+
+namespace iris {
+
+struct ngram_occurrence
+{
+    ngram_document_id doc_id;
+    int pos;
+
+    [[nodiscard]] constexpr bool operator==(ngram_occurrence const&) const noexcept = default;
+    [[nodiscard]] constexpr std::strong_ordering operator<=>(ngram_occurrence const&) const noexcept = default;
+};
+
+template<std::size_t N, class CharT>
+struct ngram
+{
+    // TODO: optimize for N=1
+    std::array<CharT, N> chars;
+
+    [[nodiscard]] constexpr bool operator==(ngram const&) const noexcept = default;
+    [[nodiscard]] constexpr std::strong_ordering operator<=>(ngram const&) const noexcept = default;
+};
+
+inline namespace ngram_literals {
+
+[[nodiscard]] constexpr ngram_document_id operator ""_doc_id(unsigned long long id) noexcept
+{
+    return ngram_document_id{static_cast<std::underlying_type_t<ngram_document_id>>(id)};
+}
+
+[[nodiscard]] constexpr auto operator ""_2gram(char32_t const* str, std::size_t len) noexcept
+{
+    assert(len == 2);
+    return ngram<2, char32_t>{str[0], str[1]};
+}
+
+} // ngram_literals
+
+
+namespace detail {
+
+template<std::size_t N, class CharT>
+struct ngram_index
+{
+    void append(ngram<N, CharT> ng, ngram_document_id doc_id, int pos)
+    {
+        occs[ng].emplace_back(doc_id, pos);
+    }
+
+    std::flat_map<ngram<N, CharT>, std::vector<ngram_occurrence>> occs;
+};
+
+template<class CharT>
+struct ngram_index_storage
+{
+    ngram_index<1, CharT> uni_idx;
+    ngram_index<2, CharT> bi_idx;
+};
+
+} // detail
+
+template<class CharT = char32_t>
+class ngram_database
+{
+public:
+    [[nodiscard]]
+    ngram_document_id add_document(std::basic_string_view<CharT> const doc_text)
+    {
+        ngram_document_id const doc_id{max_doc_id_};
+        max_doc_id_ = ngram_document_id{std::to_underlying(max_doc_id_) + 1u};
+
+        for (std::size_t i = 0; i < doc_text.size(); ++i) {
+            store_.uni_idx.append(ngram<1, CharT>{doc_text[i]}, doc_id, int(i));
+        }
+
+        auto const do_ngram = [&]<std::size_t N>(detail::ngram_index<N, CharT>& idx) {
+            if (doc_text.size() < N) return;
+
+            ngram<N, CharT> ng;
+            std::size_t i = 0;
+            for (; i < N; ++i) { // TODO: loop unroll
+                ng.chars[i] = doc_text[i];
+            }
+            idx.append(ng, doc_id, 0);
+
+            for (; i < doc_text.size(); ++i) {
+                std::ranges::shift_left(ng.chars, 1);
+                ng.chars[N - 1] = doc_text[i];
+                idx.append(ng, doc_id, int(i - N + 1));
+            }
+        };
+        do_ngram(get_index<2>());
+
+        return doc_id;
+    }
+
+    template<std::size_t N>
+    [[nodiscard]]
+    std::vector<ngram_occurrence> const* get_occurrences(ngram<N, CharT> ng) const noexcept
+    {
+        auto const& idx = get_index<N>();
+        auto const it = idx.occs.find(ng);
+        if (it == idx.occs.end()) return nullptr;
+        return &it->second;
+    }
+
+private:
+    template<std::size_t N>
+    [[nodiscard]] auto& get_index(this auto& self) noexcept
+    {
+        if constexpr (N == 1) {
+            return self.store_.uni_idx;
+        } else if constexpr (N == 2) {
+            return self.store_.bi_idx;
+        } else {
+            static_assert(false, "unhandled N");
+        }
+    }
+
+    ngram_document_id max_doc_id_{0_doc_id};
+    detail::ngram_index_storage<CharT> store_;
+};
+
+
+} // iris
+
+#endif
---
+++ test/ngram.cpp
@@ -0,0 +1,124 @@
+#include "iris_test.hpp"
+
+#include <iris/ngram.hpp>
+
+#include <format>
+#include <iostream>
+#include <ranges>
+
+#ifdef _MSC_VER
+# include <Windows.h>
+#endif
+
+namespace iris {
+
+inline std::ostream& operator<<(std::ostream& os, iris::ngram_occurrence const& occ)
+{
+    return os << std::format("{}:{}", occ.doc_id, occ.pos);
+}
+
+} // iris
+
+// --------------------------------------------------
+
+using namespace iris::ngram_literals;
+using iris::ngram_occurrence;
+
+[[nodiscard]]
+constexpr auto make_occurrences(std::initializer_list<ngram_occurrence> occs)
+{
+    return std::vector<ngram_occurrence>{occs};
+}
+
+#define IRIS_CHECK_OCCURRENCE(ng_str, ...) do { \
+        std::vector<ngram_occurrence> const* occs = nullptr; \
+        CHECK((occs = ngram_db.get_occurrences(U ## ng_str ## _2gram))); \
+        if (occs) { \
+            CHECK(*occs == make_occurrences({__VA_ARGS__})); \
+        } \
+    } while (false);
+
+TEST_CASE("ngram")
+{
+#ifdef _MSC_VER
+    SetConsoleOutputCP(CP_UTF8);
+#endif
+
+    // https://gihyo.jp/dev/serial/01/make-findspot/0005
+
+    {
+        iris::ngram_database<> ngram_db;
+        (void)ngram_db.add_document(U"今日は良い天気です。");
+
+        IRIS_CHECK_OCCURRENCE("今日", {0_doc_id, 0});
+        IRIS_CHECK_OCCURRENCE("日は", {0_doc_id, 1});
+        IRIS_CHECK_OCCURRENCE("は良", {0_doc_id, 2});
+        IRIS_CHECK_OCCURRENCE("良い", {0_doc_id, 3});
+        IRIS_CHECK_OCCURRENCE("い天", {0_doc_id, 4});
+        IRIS_CHECK_OCCURRENCE("天気", {0_doc_id, 5});
+        IRIS_CHECK_OCCURRENCE("気で", {0_doc_id, 6});
+        IRIS_CHECK_OCCURRENCE("です", {0_doc_id, 7});
+        IRIS_CHECK_OCCURRENCE("す。", {0_doc_id, 8});
+    }
+    {
+        iris::ngram_database<> ngram_db;
+        (void)ngram_db.add_document(U"今日は大雨です。");
+
+        IRIS_CHECK_OCCURRENCE("今日", {0_doc_id, 0});
+        IRIS_CHECK_OCCURRENCE("日は", {0_doc_id, 1});
+        IRIS_CHECK_OCCURRENCE("は大", {0_doc_id, 2});
+        IRIS_CHECK_OCCURRENCE("大雨", {0_doc_id, 3});
+        IRIS_CHECK_OCCURRENCE("雨で", {0_doc_id, 4});
+        IRIS_CHECK_OCCURRENCE("です", {0_doc_id, 5});
+        IRIS_CHECK_OCCURRENCE("す。", {0_doc_id, 6});
+    }
+    {
+        iris::ngram_database<> ngram_db;
+        (void)ngram_db.add_document(U"今日の東海地方は大雨でしょう。");
+
+        IRIS_CHECK_OCCURRENCE("今日", {0_doc_id, 0});
+        IRIS_CHECK_OCCURRENCE("日の", {0_doc_id, 1});
+        IRIS_CHECK_OCCURRENCE("の東", {0_doc_id, 2});
+        IRIS_CHECK_OCCURRENCE("東海", {0_doc_id, 3});
+        IRIS_CHECK_OCCURRENCE("海地", {0_doc_id, 4});
+        IRIS_CHECK_OCCURRENCE("地方", {0_doc_id, 5});
+        IRIS_CHECK_OCCURRENCE("方は", {0_doc_id, 6});
+        IRIS_CHECK_OCCURRENCE("は大", {0_doc_id, 7});
+        IRIS_CHECK_OCCURRENCE("大雨", {0_doc_id, 8});
+        IRIS_CHECK_OCCURRENCE("雨で", {0_doc_id, 9});
+        IRIS_CHECK_OCCURRENCE("でし", {0_doc_id, 10});
+        IRIS_CHECK_OCCURRENCE("しょ", {0_doc_id, 11});
+        IRIS_CHECK_OCCURRENCE("ょう", {0_doc_id, 12});
+        IRIS_CHECK_OCCURRENCE("う。", {0_doc_id, 13});
+    }
+
+    {
+        iris::ngram_database<> ngram_db;
+        (void)ngram_db.add_document(U"今日は良い天気です。");
+        (void)ngram_db.add_document(U"今日は大雨です。");
+        (void)ngram_db.add_document(U"今日の東海地方は大雨でしょう。");
+
+        IRIS_CHECK_OCCURRENCE("今日", {0_doc_id, 0}, {1_doc_id, 0}, {2_doc_id, 0});
+        IRIS_CHECK_OCCURRENCE("日は", {0_doc_id, 1}, {1_doc_id, 1});
+        IRIS_CHECK_OCCURRENCE("は良", {0_doc_id, 2});
+        IRIS_CHECK_OCCURRENCE("良い", {0_doc_id, 3});
+        IRIS_CHECK_OCCURRENCE("い天", {0_doc_id, 4});
+        IRIS_CHECK_OCCURRENCE("天気", {0_doc_id, 5});
+        IRIS_CHECK_OCCURRENCE("気で", {0_doc_id, 6});
+        IRIS_CHECK_OCCURRENCE("です", {0_doc_id, 7}, {1_doc_id, 5});
+        IRIS_CHECK_OCCURRENCE("す。", {0_doc_id, 8}, {1_doc_id, 6});
+        IRIS_CHECK_OCCURRENCE("は大", {1_doc_id, 2}, {2_doc_id, 7});
+        IRIS_CHECK_OCCURRENCE("大雨", {1_doc_id, 3}, {2_doc_id, 8});
+        IRIS_CHECK_OCCURRENCE("雨で", {1_doc_id, 4}, {2_doc_id, 9});
+        IRIS_CHECK_OCCURRENCE("日の", {2_doc_id, 1});
+        IRIS_CHECK_OCCURRENCE("の東", {2_doc_id, 2});
+        IRIS_CHECK_OCCURRENCE("東海", {2_doc_id, 3});
+        IRIS_CHECK_OCCURRENCE("海地", {2_doc_id, 4});
+        IRIS_CHECK_OCCURRENCE("地方", {2_doc_id, 5});
+        IRIS_CHECK_OCCURRENCE("方は", {2_doc_id, 6});
+        IRIS_CHECK_OCCURRENCE("でし", {2_doc_id, 10});
+        IRIS_CHECK_OCCURRENCE("しょ", {2_doc_id, 11});
+        IRIS_CHECK_OCCURRENCE("ょう", {2_doc_id, 12});
+        IRIS_CHECK_OCCURRENCE("う。", {2_doc_id, 13});
+    }
+}
