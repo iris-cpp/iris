b5818bc0fe3509b71224a4d78f1369c370fecc31
Nana Sakisaka <1901813+saki7@users.noreply.github.com>
2026-08-22 08:25:51 +0900

    Add proxy, make function CPO

--- include/iris/marshal/serialize_json.hpp
+++ include/iris/marshal/serialize_json.hpp
@@ -25,6 +25,7 @@ struct format
     static constexpr bool key = StringLike<std::remove_cvref_t<K>>;
 };
 
+template<class T> concept serializable_proxy    = marshal::serializable_proxy<T, format>;
 template<class T> concept serializable_class    = marshal::serializable_class<T, format>;
 template<class T> concept serializable_optional = marshal::serializable_optional<T, format>;
 template<class T> concept serializable_scalar   = marshal::serializable_scalar<T, format>;
@@ -33,102 +34,110 @@ template<class T> concept serializable_array    = marshal::serializable_array<T,
 template<class T> concept serializable_tuple    = marshal::serializable_tuple<T, format>;
 template<class T> concept serializable          = marshal::serializable<T, format>;
 
-template<serializable_scalar T>
-void save(nlohmann::json& out, T const& value);
+namespace detail {
 
-template<serializable_array R>
-void save(nlohmann::json& out, R const& arr);
-
-template<serializable_map MapT>
-void save(nlohmann::json& out, MapT const& map);
-
-template<serializable_tuple TupleT>
-void save(nlohmann::json& out, TupleT const& tup);
-
-template<serializable_class ClassT>
-void save(nlohmann::json& out, ClassT const& klass);
-
-template<serializable_optional OptionalT>
-void save(nlohmann::json& out, OptionalT const& opt);
-
-
-template<serializable_scalar T>
-void save(nlohmann::json& out, T const& value)
+struct save_fn
 {
-    if constexpr (std::is_enum_v<T>) {
-        out = std::to_underlying(value);
-    } else {
-        out = value;
-    }
-}
-
-template<serializable_array R>
-void save(nlohmann::json& out, R const& arr)
-{
-    auto json_arr = nlohmann::json::array();
-
-    for (auto const& elem : arr) {
-        nlohmann::json elem_json;
-        json::save(elem_json, elem);
-        json_arr.emplace_back(std::move(elem_json));
+    template<serializable_scalar T>
+    static void operator()(nlohmann::json& out, T const& value)
+    {
+        if constexpr (std::is_enum_v<T>) {
+            out = std::to_underlying(value);
+        } else {
+            out = value;
+        }
     }
 
-    out = std::move(json_arr);
-}
+    template<serializable_array R>
+    static void operator()(nlohmann::json& out, R const& arr)
+    {
+        auto json_arr = nlohmann::json::array();
 
-template<serializable_map MapT>
-void save(nlohmann::json& out, MapT const& map)
-{
-    auto json_map = nlohmann::json::object();
+        for (auto const& elem : arr) {
+            nlohmann::json elem_json;
+            save_fn{}(elem_json, elem);
+            json_arr.emplace_back(std::move(elem_json));
+        }
 
-    for (auto const& [key, value] : map) {
-        json::save(json_map[std::basic_string_view{key}], value);
+        out = std::move(json_arr);
     }
 
-    out = std::move(json_map);
-}
+    template<serializable_map MapT>
+    static void operator()(nlohmann::json& out, MapT const& map)
+    {
+        auto json_map = nlohmann::json::object();
 
-template<serializable_tuple TupleT>
-void save(nlohmann::json& out, TupleT const& tup)
-{
-    auto json_arr = nlohmann::json::array();
+        for (auto const& [key, value] : map) {
+            save_fn{}(json_map[std::basic_string_view{key}], value);
+        }
 
-    alloy::for_each(tup, [&](auto const& elem) {
-        nlohmann::json elem_json;
-        json::save(elem_json, elem);
-        json_arr.emplace_back(std::move(elem_json));
-    });
-
-    out = std::move(json_arr);
-}
-
-template<serializable_class ClassT>
-void save(nlohmann::json& out, ClassT const& klass)
-{
-    auto json_map = nlohmann::json::object();
-
-    constexpr auto const& fields = adapted_class_traits<ClassT>::fields;
-    alloy::for_each(fields, [&]<auto Mem>(detail::field_definition<Mem> const& def) {
-        json::save(json_map[def.name], (klass.*Mem)());
-    });
-
-    out = std::move(json_map);
-}
-
-template<serializable_optional OptionalT>
-void save(nlohmann::json& out, OptionalT const& opt)
-{
-    if (opt) {
-        json::save(out, *opt);
-    } else {
-        out = nullptr;
+        out = std::move(json_map);
     }
-}
+
+    template<serializable_tuple TupleT>
+    static void operator()(nlohmann::json& out, TupleT const& tup)
+    {
+        auto json_arr = nlohmann::json::array();
+
+        alloy::for_each(tup, [&](auto const& elem) {
+            nlohmann::json elem_json;
+            save_fn{}(elem_json, elem);
+            json_arr.emplace_back(std::move(elem_json));
+        });
+
+        out = std::move(json_arr);
+    }
+
+    template<serializable_class ClassT>
+    static void operator()(nlohmann::json& out, ClassT const& klass)
+    {
+        auto json_map = nlohmann::json::object();
+
+        constexpr auto const& fields = adapted_class_traits<ClassT>::fields;
+        alloy::for_each(fields, [&]<auto Mem>(marshal::detail::field_definition<Mem> const& def) {
+            save_fn{}(json_map[def.name], (klass.*Mem)());
+        });
+
+        out = std::move(json_map);
+    }
+
+    template<serializable_optional OptionalT>
+    static void operator()(nlohmann::json& out, OptionalT const& opt)
+    {
+        if (opt) {
+            save_fn{}(out, *opt);
+        } else {
+            out = nullptr;
+        }
+    }
+
+    template<serializable_proxy ProxyT>
+    static void operator()(nlohmann::json& out, ProxyT const& proxy)
+    {
+        save_fn{}(out, adapted_proxy_traits<ProxyT>::to_proxy(proxy));
+    }
+
+    template<serializable T>
+    [[nodiscard]] static nlohmann::json operator()(T const& value)
+    {
+        nlohmann::json out;
+        save_fn{}(out, value);
+        return out;
+    }
+};
 
 // ----------------------------------------------------
 
-// TODO: load
+// TODO
+struct load_fn
+{
 
+};
+
+} // detail
+
+[[maybe_unused]] inline constexpr detail::save_fn save{};
+[[maybe_unused]] inline constexpr detail::load_fn load{};
 
 } // iris::marshal::json
 
