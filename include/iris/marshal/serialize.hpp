cbd9c544aaf10e2509089f2e5fa4a799ecee8bff
Nana Sakisaka <1901813+saki7@users.noreply.github.com>
2026-08-23 14:35:38 +0900

    Make abbreviation longer

--- include/iris/marshal/serialize.hpp
+++ include/iris/marshal/serialize.hpp
@@ -14,107 +14,107 @@
 
 namespace iris::marshal {
 
-template<class W>
+template<class WriterT>
 concept writer =
     requires {
-        typename W::format;
+        typename WriterT::format;
     } &&
-    requires(W& w, std::string_view map_key) {
-        w.null();
-        w.begin_array();
-        w.end_array();
-        w.begin_object();
-        w.map_key(map_key);
-        w.end_object();
+    requires(WriterT& wr, std::string_view map_key) {
+        wr.null();
+        wr.begin_array();
+        wr.end_array();
+        wr.begin_object();
+        wr.map_key(map_key);
+        wr.end_object();
     };
 
-template<writer W>
+template<writer WriterT>
 struct basic_save_fn
 {
-    using format = W::format;
+    using format = WriterT::format;
 
     template<serializable<format> T>
         requires requires {
-            typename W::result_type;
-        } && std::constructible_from<W, typename W::result_type&>
-    static constexpr void operator()(W::result_type& res, T const& value)
+            typename WriterT::result_type;
+        } && std::constructible_from<WriterT, typename WriterT::result_type&>
+    static constexpr void operator()(WriterT::result_type& res, T const& value)
     {
-        W w{res};
-        basic_save_fn{}(w, value);
+        WriterT wr{res};
+        basic_save_fn{}(wr, value);
     }
 
     template<serializable_scalar<format> T>
-        requires requires(W& w, T const& value) { w.scalar(value); }
-    static constexpr void operator()(W& w, T const& value)
+        requires requires(WriterT& wr, T const& value) { wr.scalar(value); }
+    static constexpr void operator()(WriterT& wr, T const& value)
     {
-        w.scalar(value);
+        wr.scalar(value);
     }
 
     template<serializable_optional<format> OptionalT>
-    static constexpr void operator()(W& w, OptionalT const& opt)
+    static constexpr void operator()(WriterT& wr, OptionalT const& opt)
     {
         if (opt) {
-            basic_save_fn{}(w, *opt);
+            basic_save_fn{}(wr, *opt);
         } else {
-            w.null();
+            wr.null();
         }
     }
 
     template<serializable_proxy<format> ProxyT>
-    static constexpr void operator()(W& w, ProxyT const& proxy)
+    static constexpr void operator()(WriterT& wr, ProxyT const& proxy)
     {
-        basic_save_fn{}(w, adapted_proxy_traits<ProxyT, format>::to_native_type(proxy));
+        basic_save_fn{}(wr, adapted_proxy_traits<ProxyT, format>::to_native_type(proxy));
     }
 
     template<serializable_array<format> R>
-    static constexpr void operator()(W& w, R const& arr)
+    static constexpr void operator()(WriterT& wr, R const& arr)
     {
-        w.begin_array();
+        wr.begin_array();
         for (auto const& elem : arr) {
-            basic_save_fn{}(w, elem);
+            basic_save_fn{}(wr, elem);
         }
-        w.end_array();
+        wr.end_array();
     }
 
     template<serializable_map<format> MapT>
-    static constexpr void operator()(W& w, MapT const& map)
+    static constexpr void operator()(WriterT& wr, MapT const& map)
     {
-        w.begin_object();
+        wr.begin_object();
         for (auto const& [k, v] : map) {
             if constexpr (adapted_proxy<ranges::range_key_t<MapT>, format>) {
-                w.map_key(adapted_proxy_traits<ranges::range_key_t<MapT>, format>::to_native_type(k));
+                wr.map_key(adapted_proxy_traits<ranges::range_key_t<MapT>, format>::to_native_type(k));
             } else {
-                w.map_key(k);
+                wr.map_key(k);
             }
-            basic_save_fn{}(w, v);
+            basic_save_fn{}(wr, v);
         }
-        w.end_object();
+        wr.end_object();
     }
 
     template<serializable_tuple<format> TupleT>
-    static constexpr void operator()(W& w, TupleT const& tup)
+    static constexpr void operator()(WriterT& wr, TupleT const& tup)
     {
-        w.begin_array();
+        wr.begin_array();
         alloy::for_each(tup, [&](auto const& elem) {
-            basic_save_fn{}(w, elem);
+            basic_save_fn{}(wr, elem);
         });
-        w.end_array();
+        wr.end_array();
     }
 
     template<serializable_class<format> ClassT>
-    static constexpr void operator()(W& w, ClassT const& klass)
+    static constexpr void operator()(WriterT& wr, ClassT const& klass)
     {
-        w.begin_object();
+        wr.begin_object();
         constexpr auto const& fields = adapted_class_traits<ClassT>::fields;
         alloy::for_each(fields, [&]<class T, auto GetMem, auto SetMem>(detail::field_definition<T, GetMem, SetMem> const& def) {
             if constexpr (adapted_proxy<decltype(def.name), format>) {
-                w.map_key(adapted_proxy_traits<decltype(def.name), format>::to_native_type(def.name));
+                wr.map_key(adapted_proxy_traits<decltype(def.name), format>::to_native_type(def.name));
             } else {
-                w.map_key(def.name);
+                wr.map_key(def.name);
             }
-            basic_save_fn{}(w, (klass.*GetMem)());
+            basic_save_fn{}(wr, (klass.*GetMem)());
         });
-        w.end_object();
+        wr.end_object();
     }
 };
 
